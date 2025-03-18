deeeeb76543c8f1d884a2069fa0a3b9fe6de3933
mchangchien <matt.changchien@microsoft.com>
2025-03-18 23:12:19 +0800

    some enhancements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message /></div>;
+  let items = ["New York", "Taipei", "London", "Tokyo", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
